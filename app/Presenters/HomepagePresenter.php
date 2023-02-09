bb6a6f632478a3bde3269b2e844ad78eab7051d5
Jiri Cerny <cerny@arit.cz>
2023-02-09 09:17:28 +0100

    Added from funkacer

--- app/Presenters/HomepagePresenter.php
+++ app/Presenters/HomepagePresenter.php
@@ -8,6 +8,49 @@ use App\Model\PostFacade;
 use Nette;
 //use Nette\Application\UI\Form;
 
+class Thumbnail {
+
+	protected $id;
+	protected $menu;
+	protected $color;
+	protected $picture;
+	protected $reference;
+	protected $order;
+
+	public function __construct ($argId, $argMenu, $argColor, $argPicture, $argReference, $argOrder) {
+		$this->id = $argId;
+		$this->menu = $argMenu;
+		$this->color = $argColor;
+		$this->picture = $argPicture;
+		$this->reference = $argReference;
+		$this->order = $argOrder;
+	}
+
+	public function getId () {
+		return $this->id;
+	}
+
+	public function getMenu () {
+		return $this->menu;
+	}
+
+	public function getColor () {
+		return $this->color;
+	}
+
+	public function getPicture () {
+		return $this->picture;
+	}
+
+	public function getReference () {
+		return $this->reference;
+	}
+
+	public function getOrder () {
+		return $this->order;
+	}
+}
+
 final class HomepagePresenter extends Nette\Application\UI\Presenter
 {
     /* Původní
@@ -37,6 +80,18 @@ final class HomepagePresenter extends Nette\Application\UI\Presenter
 
 	public function renderDefault(): void
 	{
+		$poleObrazku = scandir("./img/");
+		$this->template->poleObrazku = $poleObrazku;
+
+		$poleThumbnails = [
+			'weather' => new Thumbnail ("weather", "Předpověď počasí", "black", "weather_picture.png", "/weather-app", 1),
+			'kalendar' => new Thumbnail ("kalendar", "České svátky kalendář", "black", "app_picture.png", "https://play.google.com/store/apps/details?id=funkacer.ceskesvatkykalendar", 2),
+			'penzion' => new Thumbnail ("penzion", "Prima-penzion", "black", "primapenzion-main.jpg", "/prima-penzion", 3),
+			'prevodnik' => new Thumbnail ("prevodnik", "Převodník teplot", "black", "temp_picture.png", "/prevodnik-teplot", 4),
+			'nasobilka' => new Thumbnail ("nasobilka", "Malá násobilka", "black", "nasobilka_picture.png", "/mala-nasobilka", 5)
+		];
+		$this->template->poleThumbnails = $poleThumbnails;
+
 		$this->template->posts = $this->facade
 			->getPublicArticles()
 			->limit(5);
--- app/Presenters/StatusPresenter.php
+++ app/Presenters/StatusPresenter.php
@@ -13,12 +13,15 @@ final class StatusPresenter extends Nette\Application\UI\Presenter
     public $model;
 
 	public function actionStatus() {
+        /*
         if (file_exists('temp_files/status.json')) {
             $status = file_get_contents('temp_files/status.json', TRUE);
             echo json_encode($status);
             exit;
         }
         echo json_encode(['running' => false, 'error' => 'No file found']);
+        */
+        echo json_encode(['running' => true, 'statusPercent' => date('Y-m-d - H:i:s')]);
         exit;
     }
 
